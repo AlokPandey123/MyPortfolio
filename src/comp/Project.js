fcbcee2506804011797c681373ddb99ea2d12697
AlokPandey123 <128793825+AlokPandey123@users.noreply.github.com>
2024-01-06 16:01:13 +0530

    Update Project.js

--- src/comp/Project.js
+++ src/comp/Project.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './project.css'
 import Project1 from '../img/project1.png'
+import Project1 from '../img/project2.png'
 
 
 const Project = (e) => {
@@ -10,9 +11,9 @@ const Project = (e) => {
       <h1 className="m-5"><span className="text-danger">Proj</span><span className="text-warning">ects</span></h1>
       <div className="d-flex bg-primary">
         <div id="container">
-          <img src={Project1} alt="Avatar" id="image"/>
+          <img src={Project2} alt="Avatar" id="image"/>
           <div id="overlay">
-          <button id="text"><a href="https://rural-development-website.onrender.com/">Project1</a></button>
+          <button id="text"><a href="https://coursify-rho.vercel.app/">Project1</a></button>
           </div>
         </div>
         <div id="container">
